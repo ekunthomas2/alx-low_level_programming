2341e7567bebdc30efdea0769a574dbffb3e26e0
ekunthomas2 <ekun-tee@yahoo.com>
2022-12-13 18:02:25 -0800

    0. _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,15 +3,15 @@
 
 void prin_alphabet(void);
 void print_alphabet_x10(void);
-int _isower(nt c);
+int _isower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_dgit(int);
+int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(in n);
+void print_to_98(int n);
 int _puchar(char c);
 
 #endif
