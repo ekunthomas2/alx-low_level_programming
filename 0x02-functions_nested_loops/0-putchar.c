0fa9b86428ecddc1f37fc8c966fe4c7d45fd3457
ekunthomas2 <ekun-tee@yahoo.com>
2022-12-13 17:58:04 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#include <unistd.h>
 
-void prin_alphabet(void);
-void print_alphabet_x10(void);
-int _isower(nt c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_dgit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(in n);
-
-#endif
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the characer to printt
+ *
+ * Return: n success
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
